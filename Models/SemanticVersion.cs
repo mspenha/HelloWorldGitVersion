b0649bb98552fd82377c0c270a195c2ada0b49ae
Sousa da Penha <MSousadaPenha@luxoft.com>
2020-10-06 14:18:31 +0200

    first commit

---
+++ Models/SemanticVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace HelloWorldGitVersion.Models
+{
+    public class SemanticVersion
+    {
+
+        public string VersionNumber { get; set; }
+
+        Version version = Assembly.GetEntryAssembly().GetName().Version;
+        public string GetAssemblyVersion => version.Major + "." + version.Minor + "." + version.Build;
+        public SemanticVersion()
+        {
+
+            VersionNumber = version.Major + "." + version.Minor + "." + version.Build;
+        }
+    }
+}
